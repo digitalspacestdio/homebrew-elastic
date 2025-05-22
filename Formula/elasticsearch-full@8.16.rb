2507f4aeaa6bc3dc514ad3a5f4898b565a2347da
S.Cherepanov <963777+sergeycherepanov@users.noreply.github.com>
2025-05-22 20:07:27 +0400

    Update elasticsearch-full@8.16.rb

--- Formula/elasticsearch-full@8.16.rb
+++ Formula/elasticsearch-full@8.16.rb
@@ -39,13 +39,25 @@ class ElasticsearchFullAT816 < Formula
     inreplace "#{libexec}/config/elasticsearch.yml" do |s|
       # 1. Give the cluster a unique name
       s.gsub!(/#\s*cluster\.name\: .*/, "cluster.name: #{cluster_name}")
-
+    
       # 2. Configure paths
       s.sub!(%r{#\s*path\.data: /path/to.+$}, "path.data: #{var}/lib/#{name}/")
       s.sub!(%r{#\s*path\.logs: /path/to.+$}, "path.logs: #{var}/log/#{name}/")
+    
+      # 3. Disable X-Pack security for local usage
+      unless s.include?("xpack.security.enabled")
+        s << "\nxpack.security.enabled: false\n"
+      end
     end
 
+
     inreplace "#{libexec}/config/jvm.options", %r{logs/gc.log}, "#{var}/log/#{name}/gc.log"
+
+    # Replace or insert heap settings for development
+    inreplace "#{libexec}/config/jvm.options" do |s|
+      s.gsub!(/^(-Xms).*$/, "\\1 128m") unless s.include?("-Xms128m")
+      s.gsub!(/^(-Xmx).*$/, "\\1 1g") unless s.include?("-Xmx1g")
+    end
 
     # Move config files into etc
     (etc/"#{name}").install Dir[libexec/"config/*"]
